d56dbea00dd3ec2554beee90cf6181f419178090
Panfilenka Natallia <choco-cat@mail.ru>
2020-09-29 16:20:55 +0300

    feat: add min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        return (a < b ? a : b);
+    });
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        return (a > b ? a : b);
+    });
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    return array.reduce(function (a, b) {
+        return (a + b);
+    })/array.length;
 }
